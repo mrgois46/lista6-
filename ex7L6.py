49d4396fb718db2ee4dc8bc39e7656ce2fb972c0
gois murilo <murilo1997mendes@gmail.com>
2025-06-25 21:24:28 -0300

    Create ex7L6.py

---
+++ ex7L6.py
@@ -0,0 +1,13 @@
+a = int(input("Digite um número maior que 10: "))
+
+while a<10:
+    a = int(input("Tente novamente! Digite um número maior que 10: "))
+
+print(f'{a} é maior que 10')
+
+b = int(input("Digite um número menor que 5: "))
+
+while b>5:
+    b = int(input("Tente novamente! Digite um número menor que 5: "))
+
+print(f'{b} é menor que 5')
