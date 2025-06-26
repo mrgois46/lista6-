950f5ea0da0f92a06530806fe4852858845d3a93
gois murilo <murilo1997mendes@gmail.com>
2025-06-25 21:26:29 -0300

    Create ex9L6.py

---
+++ ex9L6.py
@@ -0,0 +1,9 @@
+a = int(input("Digite um numero maior que 20: "))
+
+while a<20:
+    print("O número deve ser maior que 20!")
+    a = int(input("Digite novamente: "))
+
+while a>=1:
+    print(a)
+    a -=1
